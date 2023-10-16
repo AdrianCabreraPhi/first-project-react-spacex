88fdcad0c1f0b793bf8114915d938e53ad51e7d8
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-10-16 12:32:16 +0200

    added new map icon

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {Icon,divIcon} from "leaflet";
+import iconUrl from "../assets/map-icon.png";
 
 function Map(location) {
+
+
+  const customIcon = new Icon({
+    iconUrl: iconUrl,
+    iconSize: [38,38] // size of the icon
+  });
+
     return (
       <MapContainer center={[location.lat, location.lon]} zoom={5}   scrollWheelZoom={true}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='© OpenStreetMap contributors'
         />
-        <Marker position={[location.lat, location.lon]}>
+        <Marker position={[location.lat, location.lon]} icon={customIcon}>
           <Popup>{location.display_name}</Popup>
         </Marker>
       </MapContainer>
