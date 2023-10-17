6f35e963e37653ec16e076ce71818fc5653a68a3
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-10-17 11:30:55 +0200

    changed map to default map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import {Icon,divIcon} from "leaflet";
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon, divIcon } from "leaflet";
 import iconUrl from "../assets/map-icon.png";
 
 function Map(location) {
-
-
   const customIcon = new Icon({
     iconUrl: iconUrl,
-    iconSize: [38,38]
+    iconSize: [38, 38],
   });
 
-    return (
-      <MapContainer center={[location.lat, location.lon]} zoom={3}   scrollWheelZoom={true}>
-        <TileLayer attribution="Stadia Maps" url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"/>
-        <Marker position={[location.lat, location.lon]} icon={customIcon}>
-          <Popup>{location.display_name}</Popup>
-        </Marker>
-      </MapContainer>
-    );
-  }
+  return (
+    <MapContainer
+      center={[location.lat, location.lon]}
+      zoom={3}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[location.lat, location.lon]} icon={customIcon}>
+        <Popup>{location.display_name}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
 
-
-
-  export default Map;
+export default Map;
